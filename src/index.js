1d3483e15f19df3c975edd1eafc5681570756efc
oagomezz <ogomezbarragan437@gmail.com>
2023-02-22 14:43:32 -0800

    completed first budget template

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BudgetsContext} from "./contexts/BudgetContext"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BudgetsContext>
     <App />
+
+    </BudgetsContext>
   </React.StrictMode>
 );
 
