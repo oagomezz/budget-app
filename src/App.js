6c69385986f1930e9b4a239336eb8be0526fd251
oagomezz <ogomezbarragan437@gmail.com>
2023-04-03 10:59:57 -0700

    started budget app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import { Button, Stack } from "react-bootstrap"
 import Container from "react-bootstrap/Container"
+import AddBudgetModal from "./components/AddBudgetModal"
 import BudgetCard from "./components/BudgetCard"
-
+import { useState } from "react"
+import { useBudgets } from "./contexts/BudgetContexts"
 
 function App() {
-  return <Container className="my-4">
-    <Stack direction='horizontal' gap='2' className="mb-4">
+  const[showAddBudgetModal, setShowAddBudgetModal] = useState(false)
+  const { budgets, getBudgetExpenses } = useBudgets()
+  return( 
+  <>
+  <Container className="my-4">
+    <Stack 
+    direction='horizontal' 
+    gap='2' 
+    className="mb-4">
       <h1 className='me-auto'>Budget</h1>
-      <Button variant="primary">Add Budget</Button>
+      <Button 
+      variant="primary" 
+      onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
       <Button variant="outline-primary"> Add Expenses </Button>
       </Stack>
       <div style={{ display:'grid', gridTemplateColumns: "repeat(auto-fill,minmax(300px, 1fr))", gap: "1rem", alignItems:"Flex-start" }} >
-        <BudgetCard name='Entertainment' amount={1100} max={1000}></BudgetCard>
+        {budgets.map(budget => {
+          const amount = getBudgetExpenses(budget.id).reduce((total,expense) => total + expense.amount, 0)
+         return <BudgetCard 
+          key={budget.id}
+          name={budget.name} 
+          amount={amount} 
+          max={budget.max}></BudgetCard>
+})}
       </div>
   </Container>
-}
+  <AddBudgetModal 
+  show={showAddBudgetModal} 
+  handleClose={() => setShowAddBudgetModal(false)}/>
+  </>
+)}
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BudgetsContext} from "./contexts/BudgetContext"
+import { BudgetsProvider } from "./contexts/BudgetContexts"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BudgetsContext>
-    <App />
-
-    </BudgetsContext>
+    <BudgetsProvider>
+      <App />
+    </BudgetsProvider>
   </React.StrictMode>
 );
 
